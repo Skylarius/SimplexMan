115aedccee5b63652c94c5ee3a7ee9e1ecbd3d12
Skylarius <ilario.gerloni@gmail.com>
2019-10-17 22:39:42 +0200

    Moving platform

---
+++ SimplexMan/Assets/Scripts/MovingLoopingPlatform.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovingLoopingPlatform : MonoBehaviour {
+
+    public int speed;
+    public int maxDistance;
+    
+    private Vector3 startPosition;
+    private short int direction = 1;
+
+
+    void Start() {
+        speed = (speed) ? speed : 10;
+        maxDistance = (maxDistance) ? maxDistance : 100;
+        startPosition = transform.position;      
+    }
+
+    void FixedUpdate() {
+        transform.position+=transform.forward*direction*speed*Time.deltaTime; 
+        if (Vector3.Distance(startPosition, transform.position) > maxDistance) {
+            direction*=-1;
+        }
+    }
+
+    
+}
